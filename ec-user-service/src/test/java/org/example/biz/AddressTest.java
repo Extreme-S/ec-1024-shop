41216ece8d0951477eaf1d00a8defd43081c086c
不爱吃鱼的猫丶 <1716224950@qq.com>
2021-10-05 16:01:21 +0800

    引入单元测试模块

---
+++ ec-user-service/src/test/java/org/example/biz/AddressTest.java
@@ -0,0 +1,32 @@
+package org.example.biz;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.UserApplication;
+import org.example.model.AddressDO;
+import org.example.service.AddressService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserApplication.class)
+@Slf4j
+public class AddressTest {
+
+    @Autowired
+    private AddressService addressService;
+
+    @Test
+    public void testAddressDetail(){
+
+        AddressDO addressDO = addressService.detail(1L);
+        log.info(addressDO.toString());
+
+        Assert.assertNotNull(addressDO);
+    }
+
+}
