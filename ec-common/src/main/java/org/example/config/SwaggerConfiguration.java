d391e1e61b49457ccdf7dacf27e1488a59ada18c
不爱吃鱼的猫丶 <1716224950@qq.com>
2021-10-07 21:47:13 +0800

    增加图形验证码

--- ec-common/src/main/java/org/example/config/SwaggerConfiguration.java
+++ ec-common/src/main/java/org/example/config/SwaggerConfiguration.java
@@ -2,21 +2,28 @@ package org.example.config;
 
 import lombok.Data;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.*;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Data
 @EnableOpenApi
 public class SwaggerConfiguration {
 
+    /**
+     * 对C端用户的接口文档
+     *
+     * @return
+     */
     @Bean
     public Docket webApiDoc() {
 
@@ -30,20 +37,93 @@ public class SwaggerConfiguration {
                 //配置文档的元信息
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("org.example"))
+                .apis(RequestHandlerSelectors.basePackage("net.xdclass"))
                 //正则匹配请求路径，并分配到当前项目组
                 .paths(PathSelectors.ant("/api/**"))
-                .build();
-
+                .build()
+                // 新版SwaggerUI3.0
+                .globalRequestParameters(globalReqeustParameters())
+                .globalResponses(HttpMethod.GET, getGlabalResponseMessage())
+                .globalResponses(HttpMethod.POST, getGlabalResponseMessage());
     }
+
+
+    /**
+     * 对管理端的接口文档
+     *
+     * @return
+     */
+    @Bean
+    public Docket adminApiDoc() {
+
+        return new Docket(DocumentationType.OAS_30)
+                .groupName("管理端接口文档")
+                .pathMapping("/")
+
+                //定义是否开启Swagger，false是关闭，可以通过变量去控制，线上关闭
+                .enable(true)
+
+                //配置文档的元信息
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("net.xdclass"))
+                //正则匹配请求路径，并分配到当前项目组
+                .paths(PathSelectors.ant("/admin/**"))
+                .build();
+    }
+
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("ec-1024-shop")
+                .title("1024电商平台")
                 .description("微服务接口文档")
-                .contact(new Contact("不爱吃鱼的猫丶", "https://blog.csdn.net/cat_hate_fish", "1716224950@qq.com"))
+                .contact(new Contact("小滴课堂-二当家小D", "https://xdclass.net", "微信 xdclass6"))
                 .version("v1.0")
                 .build();
+    }
+
+
+    /**
+     * 配置全局通用参数
+     *
+     * @return
+     */
+    private List<RequestParameter> globalReqeustParameters() {
+
+        List<RequestParameter> parameters = new ArrayList<>();
+        parameters.add(new RequestParameterBuilder()
+                .name("token")
+                .description("登录令牌")
+                .in(ParameterType.HEADER)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+
+//        parameters.add(new RequestParameterBuilder()
+//                .name("token2")
+//                .description("登录令牌")
+//                .in(ParameterType.HEADER)
+//                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+//                .required(false)
+//                .build());
+
+        return parameters;
+    }
+
+
+    /**
+     * 生成通用的响应信息
+     */
+
+    private List<Response> getGlabalResponseMessage() {
+
+        List<Response> list = new ArrayList<>();
+        list.add(new ResponseBuilder()
+                .code("4xx")
+                .description("请求错误，根据code和msg检查")
+                .build());
+
+        return list;
     }
 
 }
---
+++ ec-user-service/src/main/java/org/example/controller/NotifyController.java
@@ -0,0 +1,55 @@
+package org.example.controller;
+
+import com.google.code.kaptcha.Producer;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+
+@Api(tags = "通知模块")
+@RestController
+@RequestMapping("/api/user/v1")
+@Slf4j
+public class NotifyController {
+
+    @Autowired
+    private Producer captchaProducer;
+
+    /**
+     * 获取图形验证码
+     *
+     * @param request
+     * @param response
+     */
+    @ApiOperation("获取图形验证码")
+    @GetMapping("captcha")
+    public void getCaptcha(HttpServletRequest request, HttpServletResponse response) {
+
+        String captchaText = captchaProducer.createText();
+        log.info("图形验证码:{}", captchaText);
+
+        BufferedImage bufferedImage = captchaProducer.createImage(captchaText);
+        ServletOutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            ImageIO.write(bufferedImage, "jpg", outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+            log.error("获取图形验证码异常:{}", e);
+        }
+
+    }
+
+}
