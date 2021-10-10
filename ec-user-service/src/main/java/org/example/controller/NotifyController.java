1e05e64873fd54ab3d0a7629a4ce8b1dde17a76e
不爱吃鱼的猫丶 <1716224950@qq.com>
2021-10-10 17:26:14 +0800

    用户微服务开发图形验证码加入缓存

--- ec-user-service/src/main/java/org/example/controller/NotifyController.java
+++ ec-user-service/src/main/java/org/example/controller/NotifyController.java
@@ -4,7 +4,9 @@ import com.google.code.kaptcha.Producer;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.example.util.CommonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 
 @Api(tags = "通知模块")
@@ -25,6 +28,14 @@ public class NotifyController {
 
     @Autowired
     private Producer captchaProducer;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    /**
+     * 图形验证码有效期10分钟
+     */
+    private static final long CAPTCHA_CODE_EXPIRED = 60 * 1000 * 10;
 
     /**
      * 获取图形验证码
@@ -39,6 +50,9 @@ public class NotifyController {
         String captchaText = captchaProducer.createText();
         log.info("图形验证码:{}", captchaText);
 
+        //redis存储验证码
+        redisTemplate.opsForValue().set(getCaptchaKey(request), captchaText, CAPTCHA_CODE_EXPIRED, TimeUnit.MILLISECONDS);
+
         BufferedImage bufferedImage = captchaProducer.createImage(captchaText);
         ServletOutputStream outputStream = null;
         try {
@@ -49,7 +63,25 @@ public class NotifyController {
         } catch (IOException e) {
             log.error("获取图形验证码异常:{}", e);
         }
+    }
 
+    /**
+     * 根据用户ip和userAgent信息生成图形验证码的唯一key
+     *
+     * @param request
+     * @return
+     */
+    private String getCaptchaKey(HttpServletRequest request) {
+
+        String ip = CommonUtil.getIpAddr(request);
+        String userAgent = request.getHeader("User-Agent");
+        String key = "user-service:captcha:" + CommonUtil.MD5(ip + userAgent);
+
+        log.info("ip={}", ip);
+        log.info("userAgent={}", userAgent);
+        log.info("key={}", key);
+
+        return key;
     }
 
 }
